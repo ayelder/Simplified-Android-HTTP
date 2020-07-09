d601520bf5e4330a786084f3c7517670b0e6f953
Mark Raynsford <code@io7m.com>
2020-07-09 19:10:37 +0000

    Initial attempt at continuous delivery scripts

---
+++ .ci/deploy.sh
@@ -0,0 +1,167 @@
+#!/bin/bash
+
+exec &> >(tee -a ".ci/deploy.log")
+
+#------------------------------------------------------------------------
+# Utility methods
+
+fatal()
+{
+  echo "fatal: $1" 1>&2
+  echo
+  echo "dumping log: " 1>&2
+  echo
+  cat .ci/deploy.log
+  exit 1
+}
+
+info()
+{
+  echo "info: $1" 1>&2
+}
+
+#------------------------------------------------------------------------
+# Determine version and whether or not this is a snapshot.
+#
+
+VERSION_NAME=$(./.ci/version.sh) || fatal "could not determine project version"
+VERSION_TYPE=none
+
+echo "${VERSION_NAME}" | egrep '-SNAPSHOT$'
+if [ $# -eq 0 ]
+then
+  VERSION_TYPE=snapshot
+fi
+
+VERSION_TAG=$(git describe --tags HEAD --exact-match 2>/dev/null)
+if [ ! -z "${VERSION_TAG}" ]
+then
+  VERSION_TYPE=tag
+fi
+
+#------------------------------------------------------------------------
+# Publish the built artifacts to wherever they need to go.
+#
+
+if [ "${VERSION_TYPE}" = "none" ]
+then
+  info "Current version is not a snapshot, and there is no tag. Exiting."
+  exit 0
+fi
+
+if [ "${VERSION_TYPE}" = "snapshot" ]
+then
+  info "Current version is a snapshot (${VERSION_NAME})"
+  info "Executing build"
+  ./gradlew \
+    -Dorg.gradle.internal.publish.checksums.insecure=true \
+    publish || fatal "could not publish snapshot"
+  exit 0
+fi
+
+if [ "${VERSION_TYPE}" != "tag" ]
+then
+  fatal "unrecognized version type!"
+fi
+
+info "Current version is a tag (${VERSION_TAG})"
+info "Executing build"
+
+DEPLOY_DIRECTORY="$(pwd)/deploy"
+info "Artifacts will temporarily be deployed to ${DEPLOY_DIRECTORY}"
+rm -rf "${DEPLOY_DIRECTORY}" || fatal "could not ensure temporary directory is clean"
+mkdir -p "${DEPLOY_DIRECTORY}" || fatal "could not create a temporary directory"
+
+info "Executing build"
+./gradlew \
+  -Porg.librarysimplified.directory.publish="${DEPLOY_DIRECTORY}" \
+  -Dorg.gradle.internal.publish.checksums.insecure=true \
+  publish || fatal "could not publish"
+
+#------------------------------------------------------------------------
+# Create a staging repository on Maven Central.
+#
+
+info "Creating a staging repository on Maven Central"
+
+(cat <<EOF
+create
+--description
+Simplified ${TIMESTAMP}
+--stagingProfileId
+${MAVEN_CENTRAL_STAGING_PROFILE_ID}
+--user
+${MAVEN_CENTRAL_USERNAME}
+--password
+${MAVEN_CENTRAL_PASSWORD}
+EOF
+) > args.txt || fatal "Could not write argument file"
+
+MAVEN_CENTRAL_STAGING_REPOSITORY_ID=$(java -jar brooklime.jar @args.txt) || fatal "Could not create staging repository"
+
+#------------------------------------------------------------------------
+# Upload content to the staging repository on Maven Central.
+#
+
+info "Uploading content to repository ${MAVEN_CENTRAL_STAGING_REPOSITORY_ID}"
+
+(cat <<EOF
+upload
+--stagingProfileId
+${MAVEN_CENTRAL_STAGING_PROFILE_ID}
+--user
+${MAVEN_CENTRAL_USERNAME}
+--password
+${MAVEN_CENTRAL_PASSWORD}
+--directory
+${TEMPORARY_DIRECTORY}
+--repository
+${MAVEN_CENTRAL_STAGING_REPOSITORY_ID}
+EOF
+) > args.txt || fatal "Could not write argument file"
+
+java -jar brooklime.jar @args.txt || fatal "Could not upload content"
+
+#------------------------------------------------------------------------
+# Close the staging repository.
+#
+
+info "Closing repository ${MAVEN_CENTRAL_STAGING_REPOSITORY_ID}. This can take a few minutes."
+
+(cat <<EOF
+close
+--stagingProfileId
+${MAVEN_CENTRAL_STAGING_PROFILE_ID}
+--user
+${MAVEN_CENTRAL_USERNAME}
+--password
+${MAVEN_CENTRAL_PASSWORD}
+--repository
+${MAVEN_CENTRAL_STAGING_REPOSITORY_ID}
+EOF
+) > args.txt || fatal "Could not write argument file"
+
+java -jar brooklime.jar @args.txt || fatal "Could not close staging repository"
+
+#------------------------------------------------------------------------
+# Release the staging repository.
+#
+
+info "Releasing repository ${MAVEN_CENTRAL_STAGING_REPOSITORY_ID}"
+
+(cat <<EOF
+release
+--stagingProfileId
+${MAVEN_CENTRAL_STAGING_PROFILE_ID}
+--user
+${MAVEN_CENTRAL_USERNAME}
+--password
+${MAVEN_CENTRAL_PASSWORD}
+--repository
+${MAVEN_CENTRAL_STAGING_REPOSITORY_ID}
+EOF
+) > args.txt || fatal "Could not write argument file"
+
+java -jar brooklime.jar @args.txt || fatal "Could not release staging repository"
+
+info "Release completed"
---
+++ .ci/pre.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+exec &> >(tee -a ".ci/pre.log")
+
+#------------------------------------------------------------------------
+# Utility methods
+
+fatal()
+{
+  echo "fatal: $1" 1>&2
+  echo
+  echo "dumping log: " 1>&2
+  echo
+  cat .ci/pre.log
+  exit 1
+}
+
+info()
+{
+  echo "info: $1" 1>&2
+}
+
+#------------------------------------------------------------------------
+# Download Brooklime if necessary.
+#
+
+BROOKLIME_URL="https://repo1.maven.org/maven2/com/io7m/brooklime/com.io7m.brooklime.cmdline/0.0.2/com.io7m.brooklime.cmdline-0.0.2-main.jar"
+BROOKLIME_SHA256_EXPECTED="abd775e9decd228e543c7ff1f9899183c57cc8b98e1b233e7d46ca03f4ee7e97"
+
+if [ ! -f "brooklime.jar" ]
+then
+  wget -O "brooklime.jar" "${BROOKLIME_URL}" || fatal "could not download brooklime"
+fi
+
+BROOKLIME_SHA256_RECEIVED=$(openssl sha256 "brooklime.jar" | awk '{print $NF}') || fatal "could not checksum brooklime.jar"
+
+if [ "${BROOKLIME_SHA256_EXPECTED}" != "${BROOKLIME_SHA256_RECEIVED}" ]
+then
+  fatal "brooklime.jar checksum does not match.
+  Expected: ${BROOKLIME_SHA256_EXPECTED}
+  Received: ${BROOKLIME_SHA256_RECEIVED}"
+fi
