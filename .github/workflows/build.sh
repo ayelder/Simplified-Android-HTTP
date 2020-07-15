b925c9b51eb70af2ee14248ea02f552c69cab8c8
Mark Raynsford <code@io7m.com>
2020-07-15 12:53:08 +0000

    Remove old workflows

--- .github/workflows/build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-
-
-#------------------------------------------------------------------------
-# Execute the build
-
-./gradlew clean test assemble \
-  -Dorg.gradle.internal.publish.checksums.insecure=true
