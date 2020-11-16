8da753f149c2ee0c2c6472669c2b9f3a892aa6df
Mark Raynsford <code@io7m.com>
2020-11-16 10:33:05 +0000

    Update GitHub Actions

--- .ci/build.sh
+++ .ci/build.sh
@@ -1,17 +1,11 @@
 #!/bin/bash
 
-exec &> >(tee -a ".ci/build.log")
-
 #------------------------------------------------------------------------
 # Utility methods
 
 fatal()
 {
   echo "build.sh: fatal: $1" 1>&2
-  echo
-  echo "build.sh: dumping log: " 1>&2
-  echo
-  cat .ci/build.log
   exit 1
 }
 
@@ -34,17 +28,34 @@ fi
 
 info "Executing build in '${BUILD_TYPE}' mode"
 
+JVM_ARGUMENTS="-Xmx4096m -XX:+PrintGC -XX:+PrintGCDetails -XX:MaxMetaspaceSize=512m -XX:+HeapDumpOnOutOfMemoryError -Dfile.encoding=UTF-8"
+
+info "Gradle JVM arguments: ${JVM_ARGUMENTS}"
+
 case ${BUILD_TYPE} in
   normal)
     ./gradlew \
+      -Dorg.gradle.jvmargs="${JVM_ARGUMENTS}" \
+      -Dorg.gradle.daemon=false \
+      -Dorg.gradle.parallel=false \
       -Dorg.gradle.internal.publish.checksums.insecure=true \
-      clean assembleRelease ktlint test verifySemanticVersioning || fatal "could not build"
+      ktlint assembleDebug test verifySemanticVersioning || fatal "could not build"
+
+    ./gradlew \
+      -Dorg.gradle.jvmargs="${JVM_ARGUMENTS}" \
+      -Dorg.gradle.daemon=false \
+      -Dorg.gradle.parallel=false \
+      -Dorg.gradle.internal.publish.checksums.insecure=true \
+      assembleRelease || fatal "could not build"
     ;;
 
   pull-request)
     ./gradlew \
       -Porg.librarysimplified.no_signing=true \
+      -Dorg.gradle.jvmargs="${JVM_ARGUMENTS}" \
+      -Dorg.gradle.daemon=false \
+      -Dorg.gradle.parallel=false \
       -Dorg.gradle.internal.publish.checksums.insecure=true \
-      clean assembleDebug ktlint test verifySemanticVersioning || fatal "could not build"
+      ktlint assembleDebug test verifySemanticVersioning || fatal "could not build"
     ;;
 esac
